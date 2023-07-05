d7ec2682a105a3707a0a3a2e4a7260697d4dadaf
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-07-05 12:53:08 -0700

    0. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+
+    keys = boxes[0]
+    while keys:
+        key = keys.pop(0)
+        if key < n and not unlocked[key]:
+            unlocked[key] = True
+            keys.extend(boxes[key])
+
+    return all(unlocked)
